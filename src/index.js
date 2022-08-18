a8bd3e84cfc8952af3421566f8918d105b21a062
KsanaSal <oasalivon@gmail.com>
2022-08-18 17:26:13 +0300

    First step solution task Add input listener Create fetch request Include debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import './css/styles.css';
-
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        console.log(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length === 1) {
+        console.log('country-info');
+      } else {
+        console.log('country-list');
+      }
+      console.log(data.length);
+    })
+    .catch(() => {
+      console.log('Oops, there is no country with that name');
+      //   console.log(error)
+    });
+}
+// fetchCountries('uk');
+
+input.addEventListener(
+  'input',
+  debounce(event => {
+    if (event.target.value.trim().length > 0) {
+      fetchCountries(event.target.value.trim());
+      console.log(event.target.value);
+    }
+  }, DEBOUNCE_DELAY)
+);
