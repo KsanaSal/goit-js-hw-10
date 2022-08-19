27741589854bd03ad33f65ec9b9cf0ea75f707b1
KsanaSal <oasalivon@gmail.com>
2022-08-19 13:48:57 +0300

    Add card country

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
 
+input.style.marginLeft = '40px';
+info.style.padding = '40px';
+
 function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   )
     .then(response => {
+      console.log(response);
       if (!response.ok) {
         throw new Error(response.status);
       }
@@ -18,11 +22,14 @@ function fetchCountries(name) {
     })
     .then(data => {
       console.log(data);
+      console.log(data.length);
       if (data.length > 10) {
         console.log(
           'Too many matches found. Please enter a more specific name.'
         );
       } else if (data.length === 1) {
+        countryInfo(data[0]);
+        console.log(data[0]);
         console.log('country-info');
       } else {
         countryList(data);
@@ -32,7 +39,7 @@ function fetchCountries(name) {
     })
     .catch(() => {
       console.log('Oops, there is no country with that name');
-      //   console.log(error)
+      console.log(error);
     });
 }
 // fetchCountries('uk');
@@ -55,12 +62,39 @@ function countryList(data) {
       ({
         name: { official },
         flags: { svg },
-      }) => `<li style = "list-style:none; font-size:24px;">
-      <img src="${svg}" width="50" alt="${official}"/>
+      }) => `<li style = "list-style:none; font-size:28px;">
+      <img src="${svg}" width="35" alt="${official}"/>
   ${official}
   </li>`
     )
     .join('');
   list.insertAdjacentHTML('afterbegin', countries);
   console.log(countries);
+}
+
+function countryInfo(card) {
+  info.innerHTML = '';
+  list.innerHTML = '';
+  const {
+    name: { official },
+    flags: { svg },
+    population,
+    capital,
+    languages,
+  } = card;
+  const listOfLanguages = [];
+  for (const key in languages) {
+    listOfLanguages.push(languages[key]);
+  }
+  console.log(languages);
+  const countryCard = `
+  <h1 style = "font-size:42px"><img src="${svg}" width="35" alt="${official}"> ${official}</h1>
+  <ul style = "list-style:none; padding:0; font-size:24px">
+  <li><strong>Capital:</strong> ${capital}</li>
+  <li><strong>Population:</strong> ${population}</li>
+  <li><strong>Languages:</strong> ${listOfLanguages.join(', ')}</li>
+  </ul>
+  `;
+  info.insertAdjacentHTML('afterbegin', countryCard);
+  console.log(countryCard);
 }
